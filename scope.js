3d096ff4a55fdb8220b3dbdab3642bd806d3e6f6
anishakumari5 <ajhaanni@gmail.com>
2024-10-01 17:50:56 +0530

    scope

--- scope.js
+++ scope.js
@@ -13,9 +13,25 @@ if (true)
     const numb2=98; //Won't be detected outside if block
 }
 
-console.log(numb); //Give output 52, which is inside if block
-console.log(numb1); //Will give error, but if it is declared in global scope, then it will give that particular value
-console.log(numb2); //Will give error, but if it is declared in global scope, then it will give that particular value
+// console.log(numb); //Give output 52, which is inside if block
+// console.log(numb1); //Will give error, but if it is declared in global scope, then it will give that particular value
+// console.log(numb2); //Will give error, but if it is declared in global scope, then it will give that particular value
 
 
+//Remember: Global scope in a console environment in a browser is different from that in the code terminal of node
 
+//Scope in nested function
+
+function one()
+{
+    const user1="Anisha";
+    function two()
+    {
+        console.log(user1); //This won't give any error because user1 is declared in the parent scope, so the child inherits it
+        const user2="Muskan";
+    }
+    console.log(user2); //This generates error, because user2 have been declared under the child scope and the parent cannot inherit it.
+    //two(); //This will generate error as the function two consists of a variable
+}
+
+one();
