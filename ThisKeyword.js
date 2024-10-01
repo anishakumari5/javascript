886ad26cded299b5011bf42fa8b9fdb7238ab87a
anishakumari5 <ajhaanni@gmail.com>
2024-10-01 20:59:44 +0530

    this in js

--- ThisKeyword.js
+++ ThisKeyword.js
@@ -9,8 +9,18 @@ const myObj={
     },
 }
 
-console.log(myObj.myFunction()); //Generates the output related to the above object (Anisha and 23)
-myObj.name="Muskan"; //Changes the property name of the object myObj gives the output (Muskan and 23) 
-console.log(myObj.myFunction()); //gives the output (Muskan and 23) 
-console.log(this); //Gives an empty object in the node environment --> {}
+// console.log(myObj.myFunction()); //Generates the output related to the above object (Anisha and 23)
+// myObj.name="Muskan"; //Changes the property name of the object myObj gives the output (Muskan and 23) 
+// console.log(myObj.myFunction()); //gives the output (Muskan and 23) 
+// console.log(this); //Gives an empty object in the node environment --> {}
 //Remember in the console on the browser console.log(this); gives Window object, as it is the global object there
+
+//This in function:
+function One()
+{
+    const username="Anisha";//Gives the output as a global object
+    console.log(this.username); //Gives the output as undefined, because this works only with objects
+    console.log(this);
+}
+One();
+
